8d4439ab39fee00aa9be4983e349f12964377957
maxino <maxs.campagnaro@gmail.com>
2023-11-06 02:46:56 +0100

    added customized lang changer for small screens

--- scripts/langChanger.js
+++ scripts/langChanger.js
@@ -2,32 +2,42 @@
 const startLang = Array.from(selector.options).map(opt => opt.value).find(val => lang.includes(val)) || 'en';
 changeLanguage(startLang);*/
 
-var defaultlang = document.getElementsByClassName("english");
-for(text of defaultlang){
-    text.classList.add("display")
-}
+/*   default language = eng    */
+var eng = "english";
+var ita = "italian";
 
-var itbtn = document.getElementsByClassName("lang-txt-it").item(0);
-var enbtn = document.getElementsByClassName("lang-txt-en").item(0);
+var txt = document.getElementsByClassName("lang-txt");
+var ittxt = txt.item(0);
+var entxt = txt.item(1);
+var img = document.getElementsByClassName("lang-img")
+var itimg = img.item(0);
+var enimg = img.item(1);
 var rubiks = document.getElementsByClassName("rubik");
 var usrubik = rubiks.item(0);
 var itrubik = rubiks.item(1);
+
+/* set default  */
+changeLanguage(eng);
 
 function changeLanguage(language){
     var textlistadd = document.getElementsByClassName(language);
     var textlistremove;
     
-    if(language == "italian"){
-        itbtn.classList.remove("display")
+    if(language == ita){
+        ittxt.classList.remove("display")
+        itimg.classList.remove("display")
         usrubik.classList.remove("display")
-        enbtn.classList.add("display")
+        entxt.classList.add("display")
+        enimg.classList.add("display")
         itrubik.classList.add("display")
         textlistremove = document.getElementsByClassName("english")
     }
-    else if(language == "english"){
-        enbtn.classList.remove("display")
+    else if(language == eng){
+        entxt.classList.remove("display")
+        enimg.classList.remove("display")
         itrubik.classList.remove("display")
-        itbtn.classList.add("display")
+        ittxt.classList.add("display")
+        itimg.classList.add("display")
         usrubik.classList.add("display")
         textlistremove = document.getElementsByClassName("italian")
     }

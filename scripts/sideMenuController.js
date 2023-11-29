28c94c04704cba7553df0ea05ec3ae82c2e6c151
maxino <maxs.campagnaro@gmail.com>
2023-11-29 14:57:05 +0100

    added menubutton animation for smallscreens

--- scripts/sideMenuController.js
+++ scripts/sideMenuController.js
@@ -1,12 +1,36 @@
 var sidemenu = document.getElementById("sidemenu");
+var menutoggle = document.getElementsByClassName("menu-toggle-line");
+var menubtn = document.getElementById("menu-button");
+
+function togglemenu(){
+    if(menubtn.value == "0"){
+        openmenu();
+        menubtn.value = "1";
+    }
+    else{
+        closemenu();
+        menubtn.value = "0";
+    }
+
+}
 
 function openmenu(){
     sidemenu.style.right = "0";
     sidemenu.style.opacity = "1";
+
+    menutoggle.item(0).style.transform = "translate(-5.2px, 0.5px) rotate(45deg) scale(0.8)";
+    menutoggle.item(1).style.transform = "translate(5.2px, -5px) rotate(-45deg) scale(0.8)";
+    menutoggle.item(2).style.transform = "translate(-5.2px, -0.5px) rotate(-45deg) scale(0.8)";
+    menutoggle.item(3).style.transform = "translate(5.2px, -5.9px) rotate(45deg) scale(0.8)";
 }
+
 function closemenu(){
     if(innerWidth < 750){
         sidemenu.style.right = "100vw";
         sidemenu.style.opacity = "0";
+
+        for(lines of menutoggle){
+            lines.style.transform = "none";
+        }
     }
 }
